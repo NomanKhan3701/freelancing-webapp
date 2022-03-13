02de2cab66b3f1d069704b7b5d33c2f67a6ec52d
shreyash dhamane <shreyashb.dhamane0@gmail.com>
2022-03-13 17:13:57 +0530

    commit

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,23 +1,14 @@
 import './App.css';
-import { Home, LoginSignup } from './pages/import';
+import { FindTalent, Home, LoginSignup } from './pages/import';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { Navbar } from './components/import';
 
 function App() {
   return (
-<<<<<<< HEAD
-    <BrowserRouter className="App">
-      <Routes>
-        <Route path = '/' element = {<Home />} />
-        <Route path = 'login' element = {<LoginSignup status = "login"/>} />
-        <Route path = 'signup' element = {<LoginSignup status = "signup"/>} />
-        <Route path = '*' element = {<h1>no element attached with this path</h1>}/>
-      </Routes>
-=======
     <BrowserRouter>
       <div className="App">
         <FindTalent/>
       </div>
->>>>>>> 788a0dd6551b988910ab5a9a218a67ca8b00932f
     </BrowserRouter>
   );
 }
