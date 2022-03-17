8f52e65f1a8769ae2f105867b65279f15c4f1aaf
Noman <noman.khan3701@gmail.com>
2022-03-17 21:22:03 +0530

    Fixed a little bit of css for the login page

--- client/src/pages/LogInAndSignUp/GLogin.jsx
+++ client/src/pages/LogInAndSignUp/GLogin.jsx
@@ -1,30 +1,32 @@
 import React from "react";
-import {GoogleLogin} from "react-google-login";
+import { GoogleLogin } from "react-google-login";
 import { refreshTokenSetup } from "./refreshToken";
 
-const clintId = "844752415174-c93pnbtgdm8opp0d1fcoqgpl22b4vban.apps.googleusercontent.com";
+const clintId =
+  "844752415174-c93pnbtgdm8opp0d1fcoqgpl22b4vban.apps.googleusercontent.com";
 
-function GLogin(){
-    const onSuccess = (res) => {
-        console.log('[Login Success] CurrentUser:', res.profileObj);
-        refreshTokenSetup(res);
-    }
-    const onFailure = (res) => {
-        console.log('[Login failed] res : ', res);
-    }
-    return (
-        <div>
-            <GoogleLogin
-                clientId = {clintId}
-                buttonText = "Login"
-                onSuccess = {onSuccess}
-                onFailure = {onFailure}
-                cookiePolicy = {'single_host_origin'}
-                style = {{marginTop : "100px"}}
-                isSignedIn = {true}
-                data-onsuccess="onSignIn"></GoogleLogin>
-        </div>
-    );
+function GLogin() {
+  const onSuccess = (res) => {
+    console.log("[Login Success] CurrentUser:", res.profileObj);
+    refreshTokenSetup(res);
+  };
+  const onFailure = (res) => {
+    console.log("[Login failed] res : ", res);
+  };
+  return (
+    <div>
+      <GoogleLogin
+        clientId={clintId}
+        buttonText="Login"
+        onSuccess={onSuccess}
+        onFailure={onFailure}
+        cookiePolicy={"single_host_origin"}
+        style={{ marginTop: "100px" }}
+        isSignedIn={true}
+        data-onsuccess="onSignIn"
+      ></GoogleLogin>
+    </div>
+  );
 }
 
 export default GLogin;
--- client/src/pages/LogInAndSignUp/GLogout.jsx
+++ client/src/pages/LogInAndSignUp/GLogout.jsx
@@ -1,24 +1,25 @@
 import React from "react";
-import {GoogleLogout} from "react-google-login";
+import { GoogleLogout } from "react-google-login";
 
-const clintId = "844752415174-c93pnbtgdm8opp0d1fcoqgpl22b4vban.apps.googleusercontent.com";
+const clintId =
+  "844752415174-c93pnbtgdm8opp0d1fcoqgpl22b4vban.apps.googleusercontent.com";
 
-function GLogin(){
-    const onSuccess = (res) => {
-        alert("Logout made successfully.");
-    }
-    const onFailure = (res) => {
-        console.log('[Login failed] res : ', res);
-    }
-    return (
-        <div>
-            <GoogleLogout
-            clientId = {clintId}
-            buttonText = "Logout"
-            onLogoutSuccess = {onSuccess}>
-            </GoogleLogout>
-        </div>
-    );
+function GLogin() {
+  const onSuccess = (res) => {
+    alert("Logout made successfully.");
+  };
+  const onFailure = (res) => {
+    console.log("[Login failed] res : ", res);
+  };
+  return (
+    <div>
+      <GoogleLogout
+        clientId={clintId}
+        buttonText="Logout"
+        onLogoutSuccess={onSuccess}
+      ></GoogleLogout>
+    </div>
+  );
 }
 
 export default GLogin;
