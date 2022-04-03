8a45268611c1f00d8c15cc47e73f21b7cacae92a
Noman <noman.khan3701@gmail.com>
2022-04-03 13:09:18 +0530

    Forced to commit

--- client/src/components/ChatMain/ChatMain.jsx
+++ client/src/components/ChatMain/ChatMain.jsx
@@ -1,18 +1,23 @@
 import React, { useState, useEffect } from "react";
-import { useSelector } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
 import { getDefaultMiddleware } from "@reduxjs/toolkit";
 import "./ChatMain.scss";
+import robot from "../../assets/images/Robot.gif";
 import { IoMdSend } from "react-icons/io";
 import user_img from "../../assets/images/Cha2.jpg";
 import { BsEmojiSmileFill } from "react-icons/bs";
 import EmojiPicker from "emoji-picker-react";
 import { AttachFile, Call, VideoCall } from "@material-ui/icons";
 import { io } from "socket.io-client";
+import axios from "axios";
 import LoadingSpinner from "../NormalSlider/LoadingSpinner";
 
-import { selectChatMainData } from "./../../features/chatMain/chatMainSlice";
+import {
+  update,
+  selectChatMainData,
+} from "./../../features/chatMain/chatMainSlice";
 
-var socket;
+var socket = io("http://localhost:8080");
 
 const ChatMain = (props) => {
   //disabling the serializable check
@@ -21,80 +26,120 @@ const ChatMain = (props) => {
   });
   //redux
   const chatMainData = useSelector(selectChatMainData);
+  console.log("chatMainData");
+  console.log(chatMainData);
+  const dispatch = useDispatch();
   //browser
   const sender = localStorage.getItem("username");
   //react hooks
   const [isLoading, setLoading] = useState(true);
+  const [chatData, setChatData] = useState(props.chatData);
   const [finalData, setFinalData] = useState(chatMainData);
-  const [room, setRoom] = useState();
   useEffect(() => {
-    socket = io("http://localhost:8080");
     setFinalData(chatMainData);
+  }, [chatMainData]);
+  console.log("finalData");
+  console.log(finalData);
+  const [showEmojiPicker, setShowEmojiPicker] = useState(false);
+  const [msg, setMsg] = useState("");
+
+  const username1 = "shreyash";
+  const username2 = "noman";
+  const ENDPOINT = "localhost:8080";
+  // const [room, setRoom] = useState();
+  const room = "shreyashnoman";
+
+  //to update the variable in all redux
+  const newChatMainDataFunction = (data, message) => {
+    return {
+      ...data,
+      chatData: [...data.chatData, message],
+    };
+  };
+  useEffect(() => {
+    if (finalData) {
+      setLoading(false);
+    }
+    const chatContainer = document.querySelector(".chat-main .middle-container");
+    if(chatContainer!==null){
+      chatContainer.scrollTop = chatContainer.scrollHeight;
+    } 
+
+      
+  });
+
+  useEffect(() => {
+    socket.emit("join", { username1, username2 }, (error) => {
+      if (error) {
+        alert(error);
+      }
+    });
+    return () => {
+      socket.disconnect(); //socket.emit("disconnect") gives error as sdisconnect is reserved word
+      socket.off();
+    };
+  }, [ENDPOINT]);
+  useEffect(() => {
     socket.on("message", (msg) => {
+      // for (let i = 0; i < chatData.length; i++) {
+      //   if (chatData[i].room === msg.usernames) {
+      //   }
+      // }
+      console.log(finalData);
+      console.log("message");
+      console.log(newChatMainDataFunction(finalData, msg));
+      // dispatch(update(newChatMainDataFunction(chatMainData, msg)));
       setFinalData((data) => {
         return {
           ...data,
           chatData: [...data.chatData, msg],
         };
       });
+      // setFinalData(newChatMainDataFunction(finalData, msg));
     });
-    socket.on("error", function (err) {
-      console.log(err);
-    });
-    const username1 = localStorage.getItem("username");
-    const username2 = localStorage.getItem("receiver");
-    if (username1 && username2) {
-      socket.emit("join", { username1, username2 }, (error) => {
-        if (error) {
-          alert(error);
-        }
-      });
-    }
-    socket.on("getRoomNo", (room) => {
-      setRoom(room);
-    });
-    if (username2) {
-      const chatContainer = document.querySelector(
-        ".chat-main .middle-container"
-      );
-      if (chatContainer !== null) {
-        chatContainer.scrollTop = chatContainer.scrollHeight;
-      }
-    }
-    return () => {
-      socket.disconnect(); //socket.emit("disconnect") gives error as sdisconnect is reserved word
-      socket.off();
-    };
-  }, [chatMainData]);
-  const [showEmojiPicker, setShowEmojiPicker] = useState(false);
-  const [msg, setMsg] = useState("");
-  useEffect(() => {
-    if (finalData) {
-      setLoading(false);
-    }
-    const chatContainer = document.querySelector(
-      ".chat-main .middle-container"
-    );
-    if (chatContainer !== null) {
-      chatContainer.scrollTop = chatContainer.scrollHeight;
-    }
-  }, [finalData]);
+  }, []);
   if (isLoading) {
     return <LoadingSpinner />;
   }
 
+  // socket.emit("getRoomNo", { username1, username2 }, (error) => {
+  //   if (error) {
+  //     alert(error);
+  //   }
+  // });
+  //below caused infinite loop thing in backend
+  // socket.on("getRoomNo", (rooom) => {
+  //   setRoom(rooom);
+  //   socket.emit("sendMessage", { room, message }, (error) => {
+  //     if (error) {
+  //       alert(error);
+  //     }
+  //   });
+  // });
+
+  // socket.emit("sendMessage", { room, message }, (error) => {
+  //   if (error) {
+  //     alert(error);
+  //   }
+  // });
+
+  // const message = "shreyashdhamane is meesage";
   const handleSendMsg = (msg) => {
     const message = {
       username: sender,
       message: msg,
       time: new Date(),
     };
+    console.log("new data");
+    console.log(newChatMainDataFunction(finalData, message));
+    // setFinalData(newChatMainDataFunction(finalData, message));
     setFinalData((data) => {
       return {
         ...data,
         chatData: [...data.chatData, message],
       };
     });
+    // dispatch(update(newChatMainDataFunction(chatMainData, message)));
     socket.emit("sendMessage", { room, message }, (error) => {
       if (error) {
         alert(error);
@@ -121,6 +166,10 @@ const ChatMain = (props) => {
   };
   return (
     <div className="chat-main">
+      {/* <div className="robot-container">
+        <img src={robot} alt="robot-waving" />
+      </div> */}
+
       <div className="top-container">
         <div className="user">
           <div className="user-img">
