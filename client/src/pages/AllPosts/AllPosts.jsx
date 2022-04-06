404a81faad4ab447de805bfff324ce50266ec58e
Noman <noman.khan3701@gmail.com>
2022-04-06 18:05:52 +0530

    Added a basic all work and all posts page

--- client/src/pages/AllPosts/AllPosts.jsx
+++ client/src/pages/AllPosts/AllPosts.jsx
@@ -14,6 +14,96 @@ const AllPosts = () => {
             Lorem ipsum dolor sit amet consectetur adipisicing elit. At labore
             nulla sequi reiciendis sunt iste atque cumque dicta voluptates
             asperiores, voluptas iure ad minus sit iusto nisi quisquam libero
+            magnam. Lorem ipsum dolor sit amet consectetur, adipisicing elit. Autem itaque dolores quisquam accusantium beatae ipsa fugit placeat minima, veritatis ipsum consectetur aspernatur modi omnis doloribus fuga impedit, temporibus similique quae!
+          </div>
+          <div className="status">In progress</div>
+        </div>
+        <div className="post-card">
+          <h1 className="title">Aws work</h1>
+          <div className="desc">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. At labore
+            nulla sequi reiciendis sunt iste atque cumque dicta voluptates
+            asperiores, voluptas iure ad minus sit iusto nisi quisquam libero
+            magnam.
+          </div>
+          <div className="status">In progress</div>
+        </div>
+        <div className="post-card">
+          <h1 className="title">Aws work</h1>
+          <div className="desc">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. At labore
+            nulla sequi reiciendis sunt iste atque cumque dicta voluptates
+            asperiores, voluptas iure ad minus sit iusto nisi quisquam libero
+            magnam.
+          </div>
+          <div className="status">In progress</div>
+        </div>
+        <div className="post-card">
+          <h1 className="title">Aws work</h1>
+          <div className="desc">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. At labore
+            nulla sequi reiciendis sunt iste atque cumque dicta voluptates
+            asperiores, voluptas iure ad minus sit iusto nisi quisquam libero
+            magnam.
+          </div>
+          <div className="status">In progress</div>
+        </div>
+        <div className="post-card">
+          <h1 className="title">Aws work</h1>
+          <div className="desc">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. At labore
+            nulla sequi reiciendis sunt iste atque cumque dicta voluptates
+            asperiores, voluptas iure ad minus sit iusto nisi quisquam libero
+            magnam.
+          </div>
+          <div className="status">In progress</div>
+        </div>
+        <div className="post-card">
+          <h1 className="title">Aws work</h1>
+          <div className="desc">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. At labore
+            nulla sequi reiciendis sunt iste atque cumque dicta voluptates
+            asperiores, voluptas iure ad minus sit iusto nisi quisquam libero
+            magnam.
+          </div>
+          <div className="status">In progress</div>
+        </div>
+        <div className="post-card">
+          <h1 className="title">Aws work</h1>
+          <div className="desc">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. At labore
+            nulla sequi reiciendis sunt iste atque cumque dicta voluptates
+            asperiores, voluptas iure ad minus sit iusto nisi quisquam libero
+            magnam.
+          </div>
+          <div className="status">In progress</div>
+        </div>
+        <div className="post-card">
+          <h1 className="title">Aws work</h1>
+          <div className="desc">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. At labore
+            nulla sequi reiciendis sunt iste atque cumque dicta voluptates
+            asperiores, voluptas iure ad minus sit iusto nisi quisquam libero
+            magnam.
+          </div>
+          <div className="status">In progress</div>
+        </div>
+        <div className="post-card">
+          <h1 className="title">Aws work</h1>
+          <div className="desc">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. At labore
+            nulla sequi reiciendis sunt iste atque cumque dicta voluptates
+            asperiores, voluptas iure ad minus sit iusto nisi quisquam libero
+            magnam. 
+          </div>
+          <div className="status">In progress</div>
+        </div>
+        <div className="post-card">
+          <h1 className="title">Aws work</h1>
+          <div className="desc">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. At labore
+            nulla sequi reiciendis sunt iste atque cumque dicta voluptates
+            asperiores, voluptas iure ad minus sit iusto nisi quisquam libero
             magnam.
           </div>
           <div className="status">In progress</div>
--- client/src/pages/AllWorks/AllWorks.jsx
+++ client/src/pages/AllWorks/AllWorks.jsx
@@ -18,6 +18,116 @@ const AllWorks = () => {
           </div>
           <div className="status">In progress</div>
         </div>
+        <div className="work-card">
+          <h1 className="title">Aws work</h1>
+          <div className="desc">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. At labore
+            nulla sequi reiciendis sunt iste atque cumque dicta voluptates
+            asperiores, voluptas iure ad minus sit iusto nisi quisquam libero
+            magnam.
+          </div>
+          <div className="status">In progress</div>
+        </div>
+        <div className="work-card">
+          <h1 className="title">Aws work</h1>
+          <div className="desc">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. At labore
+            nulla sequi reiciendis sunt iste atque cumque dicta voluptates
+            asperiores, voluptas iure ad minus sit iusto nisi quisquam libero
+            magnam.
+          </div>
+          <div className="status">In progress</div>
+        </div>
+        <div className="work-card">
+          <h1 className="title">Aws work</h1>
+          <div className="desc">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. At labore
+            nulla sequi reiciendis sunt iste atque cumque dicta voluptates
+            asperiores, voluptas iure ad minus sit iusto nisi quisquam libero
+            magnam.
+          </div>
+          <div className="status">In progress</div>
+        </div>
+        <div className="work-card">
+          <h1 className="title">Aws work</h1>
+          <div className="desc">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. At labore
+            nulla sequi reiciendis sunt iste atque cumque dicta voluptates
+            asperiores, voluptas iure ad minus sit iusto nisi quisquam libero
+            magnam.
+          </div>
+          <div className="status">In progress</div>
+        </div>
+        <div className="work-card">
+          <h1 className="title">Aws work</h1>
+          <div className="desc">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. At labore
+            nulla sequi reiciendis sunt iste atque cumque dicta voluptates
+            asperiores, voluptas iure ad minus sit iusto nisi quisquam libero
+            magnam.
+          </div>
+          <div className="status">In progress</div>
+        </div>
+        <div className="work-card">
+          <h1 className="title">Aws work</h1>
+          <div className="desc">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. At labore
+            nulla sequi reiciendis sunt iste atque cumque dicta voluptates
+            asperiores, voluptas iure ad minus sit iusto nisi quisquam libero
+            magnam.
+          </div>
+          <div className="status">In progress</div>
+        </div>
+        <div className="work-card">
+          <h1 className="title">Aws work</h1>
+          <div className="desc">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. At labore
+            nulla sequi reiciendis sunt iste atque cumque dicta voluptates
+            asperiores, voluptas iure ad minus sit iusto nisi quisquam libero
+            magnam.
+          </div>
+          <div className="status">In progress</div>
+        </div>
+        <div className="work-card">
+          <h1 className="title">Aws work</h1>
+          <div className="desc">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. At labore
+            nulla sequi reiciendis sunt iste atque cumque dicta voluptates
+            asperiores, voluptas iure ad minus sit iusto nisi quisquam libero
+            magnam.
+          </div>
+          <div className="status">In progress</div>
+        </div>
+        <div className="work-card">
+          <h1 className="title">Aws work</h1>
+          <div className="desc">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. At labore
+            nulla sequi reiciendis sunt iste atque cumque dicta voluptates
+            asperiores, voluptas iure ad minus sit iusto nisi quisquam libero
+            magnam.
+          </div>
+          <div className="status">In progress</div>
+        </div>
+        <div className="work-card">
+          <h1 className="title">Aws work</h1>
+          <div className="desc">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. At labore
+            nulla sequi reiciendis sunt iste atque cumque dicta voluptates
+            asperiores, voluptas iure ad minus sit iusto nisi quisquam libero
+            magnam.
+          </div>
+          <div className="status">In progress</div>
+        </div>
+        <div className="work-card">
+          <h1 className="title">Aws work</h1>
+          <div className="desc">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. At labore
+            nulla sequi reiciendis sunt iste atque cumque dicta voluptates
+            asperiores, voluptas iure ad minus sit iusto nisi quisquam libero
+            magnam.
+          </div>
+          <div className="status">In progress</div>
+        </div>
       </div>
     </div>
   );
