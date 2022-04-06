3dad337fe58086bad31bc7f92817a115f240a3af
Noman <noman.khan3701@gmail.com>
2022-04-06 20:13:23 +0530

    Started working on Client project completion dashboard

---
+++ client/src/pages/ClientProjectProgress/ClientProjectProgress.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { Navbar } from "../../components/import";
+import "./ClientProjectProgress.scss";
+
+const ClientProjectProgress = () => {
+  return (
+    <div className="client-project-progress">
+      <Navbar /> 
+      ClientProjectProgress
+    </div>
+  );
+};
+
+export default ClientProjectProgress;
