0a6cdf146bf7373c202caf75061f1e33cbb08fdd
Noman <noman.khan3701@gmail.com>
2022-04-07 18:28:30 +0530

    Added Chat and terminate options to card in allwork and allpost

--- client/src/pages/AllWorks/AllWorks.jsx
+++ client/src/pages/AllWorks/AllWorks.jsx
@@ -1,137 +1,210 @@
 import React from "react";
 import { Footer, Navbar } from "../../components/import";
 import "./AllWorks.scss";
+import { Link } from "react-router-dom";
 
 const AllWorks = () => {
   return (
     <>
-    <div className="all-works-container">
-      <Navbar />
-      <h1>All works</h1>
-      <div className="work-cards">
-        <div className="work-card">
-          <h1 className="title">Aws work</h1>
-          <div className="desc">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. At labore
-            nulla sequi reiciendis sunt iste atque cumque dicta voluptates
-            asperiores, voluptas iure ad minus sit iusto nisi quisquam libero
-            magnam.
+      <div className="all-works-container">
+        <Navbar />
+        <h1>All works</h1>
+        <div className="work-cards">
+          <div className="work-card">
+            <h1 className="title">Aws work</h1>
+            <div className="desc">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. At labore
+              nulla sequi reiciendis sunt iste atque cumque dicta voluptates
+              asperiores, voluptas iure ad minus sit iusto nisi quisquam libero
+              magnam.
+            </div>
+            <div className="btn-container">
+              <Link to="/chat" className="btn">
+                Chat
+              </Link>
+              <div className="status progress">In progress</div>
+              <Link to="/chat" className="btn">
+                Terminate
+              </Link>
+            </div>
           </div>
-          <div className="status progress">In progress</div>
-        </div>
-        <div className="work-card">
-          <h1 className="title">Aws work</h1>
-          <div className="desc">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. At labore
-            nulla sequi reiciendis sunt iste atque cumque dicta voluptates
-            asperiores, voluptas iure ad minus sit iusto nisi quisquam libero
-            magnam.
+          <div className="work-card">
+            <h1 className="title">Aws work</h1>
+            <div className="desc">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. At labore
+              nulla sequi reiciendis sunt iste atque cumque dicta voluptates
+              asperiores, voluptas iure ad minus sit iusto nisi quisquam libero
+              magnam.
+            </div>
+            <div className="btn-container">
+              <Link to="/chat" className="btn">
+                Chat
+              </Link>
+              <div className="status done">Done</div>
+            </div>
           </div>
-          <div className="status done">Done</div>
-        </div>
-        <div className="work-card">
-          <h1 className="title">Aws work</h1>
-          <div className="desc">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. At labore
-            nulla sequi reiciendis sunt iste atque cumque dicta voluptates
-            asperiores, voluptas iure ad minus sit iusto nisi quisquam libero
-            magnam.
+          <div className="work-card">
+            <h1 className="title">Aws work</h1>
+            <div className="desc">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. At labore
+              nulla sequi reiciendis sunt iste atque cumque dicta voluptates
+              asperiores, voluptas iure ad minus sit iusto nisi quisquam libero
+              magnam.
+            </div>
+            <div className="btn-container">
+              <Link to="/chat" className="btn">
+                Chat
+              </Link>
+              <div className="status progress">In progress</div>
+              <Link to="/chat" className="btn">
+                Terminate
+              </Link>
+            </div>
           </div>
-          <div className="status progress">In progress</div>
-        </div>
-        <div className="work-card">
-          <h1 className="title">Aws work</h1>
-          <div className="desc">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. At labore
-            nulla sequi reiciendis sunt iste atque cumque dicta voluptates
-            asperiores, voluptas iure ad minus sit iusto nisi quisquam libero
-            magnam.
+          <div className="work-card">
+            <h1 className="title">Aws work</h1>
+            <div className="desc">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. At labore
+              nulla sequi reiciendis sunt iste atque cumque dicta voluptates
+              asperiores, voluptas iure ad minus sit iusto nisi quisquam libero
+              magnam.
+            </div>
+            <div className="btn-container">
+              <Link to="/chat" className="btn">
+                Chat
+              </Link>
+              <div className="status done">Done</div>
+            </div>
           </div>
-          <div className="status done">Done</div>
-        </div>
-        <div className="work-card">
-          <h1 className="title">Aws work</h1>
-          <div className="desc">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. At labore
-            nulla sequi reiciendis sunt iste atque cumque dicta voluptates
-            asperiores, voluptas iure ad minus sit iusto nisi quisquam libero
-            magnam.
+          <div className="work-card">
+            <h1 className="title">Aws work</h1>
+            <div className="desc">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. At labore
+              nulla sequi reiciendis sunt iste atque cumque dicta voluptates
+              asperiores, voluptas iure ad minus sit iusto nisi quisquam libero
+              magnam.
+            </div>
+            <div className="btn-container">
+              <Link to="/chat" className="btn">
+                Chat
+              </Link>
+              <div className="status done">Done</div>
+            </div>
           </div>
-          <div className="status done">Done</div>
-        </div>
-        <div className="work-card">
-          <h1 className="title">Aws work</h1>
-          <div className="desc">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. At labore
-            nulla sequi reiciendis sunt iste atque cumque dicta voluptates
-            asperiores, voluptas iure ad minus sit iusto nisi quisquam libero
-            magnam.
+          <div className="work-card">
+            <h1 className="title">Aws work</h1>
+            <div className="desc">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. At labore
+              nulla sequi reiciendis sunt iste atque cumque dicta voluptates
+              asperiores, voluptas iure ad minus sit iusto nisi quisquam libero
+              magnam.
+            </div>
+            <div className="btn-container">
+              <Link to="/chat" className="btn">
+                Chat
+              </Link>
+              <div className="status done">Done</div>
+            </div>
           </div>
-          <div className="status done">Done</div>
-        </div>
-        <div className="work-card">
-          <h1 className="title">Aws work</h1>
-          <div className="desc">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. At labore
-            nulla sequi reiciendis sunt iste atque cumque dicta voluptates
-            asperiores, voluptas iure ad minus sit iusto nisi quisquam libero
-            magnam.
+          <div className="work-card">
+            <h1 className="title">Aws work</h1>
+            <div className="desc">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. At labore
+              nulla sequi reiciendis sunt iste atque cumque dicta voluptates
+              asperiores, voluptas iure ad minus sit iusto nisi quisquam libero
+              magnam.
+            </div>
+            <div className="btn-container">
+              <Link to="/chat" className="btn">
+                Chat
+              </Link>
+              <div className="status progress">In progress</div>
+              <Link to="/chat" className="btn">
+                Terminate
+              </Link>
+            </div>
           </div>
-          <div className="status progress">In progress</div>
-        </div>
-        <div className="work-card">
-          <h1 className="title">Aws work</h1>
-          <div className="desc">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. At labore
-            nulla sequi reiciendis sunt iste atque cumque dicta voluptates
-            asperiores, voluptas iure ad minus sit iusto nisi quisquam libero
-            magnam.
+          <div className="work-card">
+            <h1 className="title">Aws work</h1>
+            <div className="desc">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. At labore
+              nulla sequi reiciendis sunt iste atque cumque dicta voluptates
+              asperiores, voluptas iure ad minus sit iusto nisi quisquam libero
+              magnam.
+            </div>
+            <div className="btn-container">
+              <Link to="/chat" className="btn">
+                Chat
+              </Link>
+              <div className="status done">Done</div>
+            </div>
           </div>
-          <div className="status done">Done</div>
-        </div>
-        <div className="work-card">
-          <h1 className="title">Aws work</h1>
-          <div className="desc">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. At labore
-            nulla sequi reiciendis sunt iste atque cumque dicta voluptates
-            asperiores, voluptas iure ad minus sit iusto nisi quisquam libero
-            magnam.
+          <div className="work-card">
+            <h1 className="title">Aws work</h1>
+            <div className="desc">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. At labore
+              nulla sequi reiciendis sunt iste atque cumque dicta voluptates
+              asperiores, voluptas iure ad minus sit iusto nisi quisquam libero
+              magnam.
+            </div>
+            <div className="btn-container">
+              <Link to="/chat" className="btn">
+                Chat
+              </Link>
+              <div className="status done">Done</div>
+            </div>
           </div>
-          <div className="status done">Done</div>
-        </div>
-        <div className="work-card">
-          <h1 className="title">Aws work</h1>
-          <div className="desc">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. At labore
-            nulla sequi reiciendis sunt iste atque cumque dicta voluptates
-            asperiores, voluptas iure ad minus sit iusto nisi quisquam libero
-            magnam.
+          <div className="work-card">
+            <h1 className="title">Aws work</h1>
+            <div className="desc">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. At labore
+              nulla sequi reiciendis sunt iste atque cumque dicta voluptates
+              asperiores, voluptas iure ad minus sit iusto nisi quisquam libero
+              magnam.
+            </div>
+            <div className="btn-container">
+              <Link to="/chat" className="btn">
+                Chat
+              </Link>
+              <div className="status done">Done</div>
+            </div>
           </div>
-          <div className="status done">Done</div>
-        </div>
-        <div className="work-card">
-          <h1 className="title">Aws work</h1>
-          <div className="desc">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. At labore
-            nulla sequi reiciendis sunt iste atque cumque dicta voluptates
-            asperiores, voluptas iure ad minus sit iusto nisi quisquam libero
-            magnam.
+          <div className="work-card">
+            <h1 className="title">Aws work</h1>
+            <div className="desc">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. At labore
+              nulla sequi reiciendis sunt iste atque cumque dicta voluptates
+              asperiores, voluptas iure ad minus sit iusto nisi quisquam libero
+              magnam.
+            </div>
+            <div className="btn-container">
+              <Link to="/chat" className="btn">
+                Chat
+              </Link>
+              <div className="status done">Done</div>
+            </div>
           </div>
-          <div className="status done">Done</div>
-        </div>
-        <div className="work-card">
-          <h1 className="title">Aws work</h1>
-          <div className="desc">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. At labore
-            nulla sequi reiciendis sunt iste atque cumque dicta voluptates
-            asperiores, voluptas iure ad minus sit iusto nisi quisquam libero
-            magnam.
+          <div className="work-card">
+            <h1 className="title">Aws work</h1>
+            <div className="desc">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. At labore
+              nulla sequi reiciendis sunt iste atque cumque dicta voluptates
+              asperiores, voluptas iure ad minus sit iusto nisi quisquam libero
+              magnam.
+            </div>
+            <div className="btn-container">
+              <Link to="/chat" className="btn">
+                Chat
+              </Link>
+              <div className="status progress">In progress</div>
+              <Link to="/chat" className="btn">
+                Terminate
+              </Link>
+            </div>
           </div>
-          <div className="status progress">In progress</div>
         </div>
       </div>
-    </div>
-    <Footer/>
+      <Footer />
     </>
   );
 };
