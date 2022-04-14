4682fb7c95326970d1e57076520510bf11034a1b
Noman <noman.khan3701@gmail.com>
2022-04-14 10:12:03 +0530

    Little tweeks

--- client/src/pages/Bid/Bid.jsx
+++ client/src/pages/Bid/Bid.jsx
@@ -238,7 +238,7 @@ const Bid = () => {
                         <img src={clientImg} alt="user image" />
                         <div className="user-name">{bid.username}</div>
                       </div>
-                      <div className="flex">
+                      <div className="flex btn-center">
                         <div className="price">Price : {bid.amount}₹</div>
                         <div className="star">Rating : 4.9</div>
                       </div>
