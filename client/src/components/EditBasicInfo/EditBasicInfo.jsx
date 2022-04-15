d3f18adca075902af2fce94d7549e101dc49960c
Tarun <tarun.agrawal@spit.ac.in>
2022-04-15 21:37:18 +0530

    edit profile changed

--- client/src/components/EditBasicInfo/EditBasicInfo.jsx
+++ client/src/components/EditBasicInfo/EditBasicInfo.jsx
@@ -1,16 +1,16 @@
 import React from "react";
-// import './EditBasicInfo.scss'
+import './EditBasicInfo.scss'
 
 const EditBasicInfo = () => {
-  return (
-    <div className="edit-info-title">
-      <h3>Enter Full Name</h3>
-      <input type="text" placeholder="Full Name" className="input" />
-      <h3>Enter Email</h3>
-      <input type="text" placeholder="Email ID" className="input" />
-      <div className="btn">Submit</div>
-    </div>
-  );
+    return (
+        <div className="edit-info-title">
+            <h3>Enter Full Name</h3>
+            <input type="text" placeholder="Full Name" className="input" />
+            <h3>Enter Email</h3>
+            <input type="text" placeholder="Email ID" className="input" />
+            <div className="btn">Change Information</div>
+        </div>
+    );
 };
 
 export default EditBasicInfo;
