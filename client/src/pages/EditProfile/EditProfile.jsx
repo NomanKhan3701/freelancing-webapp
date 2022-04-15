9f3df2d20b3a51656a26924301fef68a9fe7584a
shreyash dhamane <shreyashb.dhamane0@gmail.com>
2022-04-15 15:56:28 +0530

    safety commit

--- client/src/pages/EditProfile/EditProfile.jsx
+++ client/src/pages/EditProfile/EditProfile.jsx
@@ -1,57 +1,63 @@
-import React, { useState } from 'react'
-import { Navbar } from '../../components/import'
-import './EditProfile.scss'
+import React, { useState } from "react";
+import { Navbar } from "../../components/import";
+import "./EditProfile.scss";
 import {
-    BrowserRouter as Router,
-    Routes,
-    Route,
-    Link,
-    Outlet
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Link,
+  Outlet,
 } from "react-router-dom";
 const EditProfile = () => {
-
-    const [sidenNavElements, setSidenNavElements] = useState([{ elemName: "Edit Username and Password", active: false, path: "edituandp" },
+  const [sidenNavElements, setSidenNavElements] = useState([
+    {
+      elemName: "Edit Username and Password",
+      active: false,
+      path: "edituandp",
+    },
     { elemName: "Edit Basic Information", active: false, path: "editbasic" },
-    { elemName: "Edit Profile", active: false, path: "editprofileinfo" }])
+    { elemName: "Edit Profile", active: false, path: "editprofileinfo" },
+  ]);
 
-    const clickLiHandler = (elem) => {
-        setSidenNavElements(() => {
-            return sidenNavElements.map((navElem) => {
-                if (navElem === elem) {
-                    return { ...navElem, active: true }
-                }
-                else {
-                    return { ...navElem, active: false }
-                }
-            })
-        })
-    }
+  const clickLiHandler = (elem) => {
+    setSidenNavElements(() => {
+      return sidenNavElements.map((navElem) => {
+        if (navElem === elem) {
+          return { ...navElem, active: true };
+        } else {
+          return { ...navElem, active: false };
+        }
+      });
+    });
+  };
 
+  return (
+    <>
+      <Navbar />
+      <div className="container">
+        <div className="sidebar">
+          <ul>
+            {sidenNavElements.map((elem, index) => {
+              return (
+                <Link to={elem.path}>
+                  <li
+                    key={index}
+                    className={elem.active ? "active" : ""}
+                    onClick={() => clickLiHandler(elem)}
+                  >
+                    {elem.elemName}
+                  </li>
+                </Link>
+              );
+            })}
+          </ul>
+        </div>
+        <div className="edit-component">
+          <Outlet />
+        </div>
+      </div>
+    </>
+  );
+};
 
-    return (
-        <>
-            <Navbar />
-            <div className="container">
-                <div className="sidebar" >
-                    <ul >
-                        {sidenNavElements.map((elem, index) => {
-                            return (
-                                <Link to={elem.path}>
-                                    <li key={index} className={elem.active ? "active" : ""}
-                                        onClick={() => clickLiHandler(elem)}>
-                                        {elem.elemName}
-                                    </li>
-                                </Link>
-                            )
-                        })}
-                    </ul>
-                </div>
-                <div className="edit-component">
-                    <Outlet />
-                </div>
-            </div>
-        </>
-    )
-}
-
-export default EditProfile
+export default EditProfile;
